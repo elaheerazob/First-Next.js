d10575dff14b62d862bb76b6a4a3ce70a48cf215
elaheerazob <ridwanelahee282@gmail.com>
2023-09-19 00:15:40 +0600

    middleware

---
+++ src/middleware.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+export function middleware (request){
+   return NextResponse.redirect(new URL("/login",request.url))
+}
+
+export const config={
+    source:["/about/:path*", "/studentlist/:path*"]
+}
