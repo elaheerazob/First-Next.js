fa78f092cbddd2325d6727f29c5d465261d4a4b2
elaheerazob <ridwanelahee282@gmail.com>
2023-10-01 00:59:03 +0600

    add lodader in page

---
+++ src/app/user/page.js
@@ -0,0 +1,23 @@
+
+async function UserList(){
+    let data =await fetch("https://dummyjson.com/products");
+    data =await data.json();
+    return data.products;
+}
+
+
+export default async function Page(){
+    let products= await UserList();
+    console.log(products);
+    return(
+        <div>
+            {
+                products.map((item)=>(
+                    <div>
+                        <h1>Name: {item.title}</h1>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
